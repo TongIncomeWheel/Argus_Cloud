4953189b8ff09944afc0be16d30b7347d03650ad
TongIncomeWheel <ash.tzl@gmail.com>
2026-04-09 21:17:02 +0800

    Fix syntax error in initial_sidebar_state

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.set_page_config(
     page_title="ARGUS",
     page_icon="📊",
     layout="wide",
-    initial_sidebar_state="collapsed"h
+    initial_sidebar_state="collapsed"
 )
 
 # ---------------------------------------------------------------------------
